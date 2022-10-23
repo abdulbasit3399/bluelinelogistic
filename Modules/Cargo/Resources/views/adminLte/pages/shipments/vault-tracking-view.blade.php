640a875a3f16d75e2db03d24a432803bb176fc8c
abdulbasit3399 <mebasit11@gmail.com>
2022-10-24 00:05:13 +0500

    fix

--- Modules/Cargo/Resources/views/adminLte/pages/shipments/vault-tracking-view.blade.php
+++ Modules/Cargo/Resources/views/adminLte/pages/shipments/vault-tracking-view.blade.php
@@ -28,9 +28,7 @@
                                 <div class="col-md-6">
                                     <div class="alert alert-danger alert-dismissible fade show" role="alert">
                                 <strong>{{\Session::get('error')}}</strong>
-                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
+                                
                                 </div>
                                 </div>
                                 @endif
